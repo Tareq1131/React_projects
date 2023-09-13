c3de99f2068070ddaa9beda25bbf6b48cc63b83b
Tareq1131 <tareqislam076@gmail.com>
2023-09-13 23:58:33 +0600

    delete itemm work properly

--- src/component/todoreact/todo.js
+++ src/component/todoreact/todo.js
@@ -1,51 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./style.css";
 
-
 const Todo = () => {
+  const [inputData, setInputDta] = useState("");
+  const [items, setItems] = useState([]);
+  //console.log(inputData);
+  //console.log(items);
+
+
+//add the items function
+  const addItem = () => {
+    if (!inputData) {
+      alert("please fill the data...");
+    } else {
+      const myNewInputData={
+        id: new Date().getTime().toString(),
+        name: inputData,
+      }
+      // setItems([...items, inputData]);
+      setItems([...items, myNewInputData]);
+      setInputDta ("");
+    }
+  };
+//delete items using this function
+  const deleteItem =(index)=>{
+    const updateItem = items.filter( (curElem)=>{
+        return curElem.id !== index;
+
+    })
+    setItems(updateItem);
+  }
+
   return (
     <>
-      <div className='main-div'>
-            <div className='child-div'>
-                <figure>
-                    <img src='./images/todo.svg' alt='image'/>
-                    <figcaption>Add Your List Here</figcaption>
-                </figure>
-                <div className='addItems'>
+      <div className="main-div">
+        <div className="child-div">
+          <figure>
+            <img src="./images/todo.svg" alt="image" />
+            <figcaption>Add Your List Here</figcaption>
+          </figure>
+          <div className="addItems">
+            <input
+              type="text"
+              placeholder="✍ Add Item"
+              className="form-control"
+              value={inputData}
+              onChange={(even) => setInputDta(even.target.value)}
+            />
 
-                    <input type='text'
-                    placeholder="✍ Add Item"
-                    className='form-control'
-                    />
-                     <i className="far fa-edit add-btn"></i>
-                     {/* <i className="fa fa-plus add-btn"></i> */}
-                </div>
-                {/* show items */}
-                <div className="showItems">
-                    <div className="eachItem">
-                        <h>Apple</h>
-                        <div className='todo-btn'>
-                            <i className="far fa-edit add-btn"></i>
-                            <i className="far fa-trash-alt add-btn"></i>
-                        </div>
+            {/* <i className="far fa-edit add-btn"></i> */}
+            <i className="fa fa-plus add-btn" onClick ={addItem }></i>
+          </div>
+          {/* show items */}
+          <div className="showItems">
+            {
+                items.map( (curElem)=>{
+                  return(
+                    <div className="eachItem" key={curElem.id}>
+                    <h>{curElem.name}</h>
+                    <div className="todo-btn">
+                      <i className="far fa-edit add-btn"></i>
+                      <i className="far fa-trash-alt add-btn"
+                       onClick={()=> deleteItem(curElem.id)}></i>
                     </div>
-                </div>
+                  </div>
+                
+                  )
+                })
 
-                {/* remove all button */}
-                <div className="showItems">
+            }
+        
+        </div>
+          {/* remove all button */}
+          <div className="showItems">
             <button
               className="btn effect04"
               data-sm-link-text="Remove All"
-             // onClick={removeAll}
-              >
+              // onClick={removeAll}
+            >
               <span> CHECK LIST</span>
             </button>
           </div>
-            </div>
-
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
